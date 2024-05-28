d5bc60d59632b4c158dc3db3c42570f2d03091c8
João Gabriel <joaogabrielnunes080@gmail.com>
2024-05-28 16:32:55 -0300

    16:26

--- argendex_cell/Pages/AddTarefa/Addtarefa.js
+++ argendex_cell/Pages/AddTarefa/Addtarefa.js
@@ -2,54 +2,187 @@ import React, { useState } from "react";
 import { TextInput, KeyboardAvoidingView, TouchableOpacity, Text, ScrollView, Image, View } from "react-native";
 import styles from "./Style_Add.js";
 import useHeaderOptions from '../../components/Header.js'; // Importando o hook do header
+import { DateTimePicker } from "@react-native-community/datetimepicker";
 
 const Addtarefa = ({ navigation }) => {
-  const [email, setEmail] = useState('');
-  const [Senha, setSenha] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+    const [tasks, setTasks] = useState({});
+    const [newTask, setNewTask] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [time, setTime] = useState(new Date());
+    const [showTimePicker, setShowTimePicker] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [tasksForSelectedDate, setTasksForSelectedDate] = useState([]);
+    const [selectedTaskIndex, setSelectedTaskIndex] = useState(null);
+    const [editModalVisible, setEditModalVisible] = useState(false);
+    const [editedTask, setEditedTask] = useState(null);
 
-  const handleLogin = () => {
-    console.log("Email", email);
-    setEmail('');
-    console.log("Senha", Senha);
-    setSenha('');
-  };
-  useHeaderOptions();
+    const handleDayPress = (day) => {
+        setSelectedDate(day.dateString);
+        if (tasks[day.dateString]) {
+            const sortedTasks = [...tasks[day.dateString]].sort((a, b) => {
+                const [aHours, aMinutes] = a.time.split(':').map(Number);
+                const [bHours, bMinutes] = b.time.split(':').map(Number);
+                return aHours - bHours || aMinutes - bMinutes;
+            });
+            setTasksForSelectedDate(sortedTasks);
+            setModalVisible(true);
+        }
+    };
+
+    const addTask = () => {
+        if (newTask.trim() !== '') {
+            const newTasks = { ...tasks };
+            if (!newTasks[selectedDate]) {
+                newTasks[selectedDate] = [];
+            }
+            newTasks[selectedDate].push({
+                title: newTask,
+                description,
+                category,
+                time: time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+            });
+            setTasks(newTasks);
+            setNewTask('');
+            setDescription('');
+            setCategory('');
+            setTime(new Date());
+        }
+    };
+
+    const editTask = (editedTask) => {
+        const updatedTasks = { ...tasks };
+        updatedTasks[selectedDate][selectedTaskIndex] = editedTask;
+        setTasks(updatedTasks);
+        setSelectedTaskIndex(null);
+        setEditModalVisible(false);
+    };
+
+    const deleteTask = () => {
+        const updatedTasks = { ...tasks };
+        updatedTasks[selectedDate].splice(selectedTaskIndex, 1);
+        if (updatedTasks[selectedDate].length === 0) {
+            delete updatedTasks[selectedDate]; // Remove a entrada do objeto se não houver mais tarefas para o dia selecionado
+        }
+        setTasks(updatedTasks);
+        setSelectedTaskIndex(null);
+    };
+
+    const handleEditTask = (index) => {
+        setEditedTask(tasksForSelectedDate[index]);
+        setSelectedTaskIndex(index);
+        setEditModalVisible(true);
+    };
+
+    const handleDeleteTask = (index) => {
+        Alert.alert(
+            'Excluir Tarefa',
+            'Tem certeza que deseja excluir esta tarefa?',
+            [
+                { text: 'Cancelar', style: 'cancel' },
+                { text: 'Confirmar', onPress: () => deleteTask() }
+            ],
+            { cancelable: false }
+        );
+    };
+
+    useHeaderOptions();
+
+    const renderCustomDay = ({ date, state }) => {
+        const dayOfWeek = new Date(date.year, date.month - 1, date.day).getDay();
+        const isWeekend = dayOfWeek === 0 || dayOfWeek === 6;
+        const isSelected = date.dateString === selectedDate;
+        const hasTasks = tasks[date.dateString];
+        const isToday = date.dateString === new Date().toISOString().split('T')[0];
+
+        return (
+            <TouchableOpacity onPress={() => handleDayPress(date)}>
+                <View style={{
+                    backgroundColor: isSelected ? (isToday ? 'green' : isWeekend ? 'red' : 'green') : 'transparent',
+                    borderRadius: 15,
+                    padding: 5
+                }}>
+                    <Text style={{
+                        color: isToday ? (isSelected ? 'black' : 'green') : state === 'disabled' ? 'grey' : isSelected ? (isWeekend ? 'black' : 'white') : isWeekend ? 'red' : 'white',
+                        textAlign: 'center'
+                    }}>
+                        {date.day}
+                    </Text>
+                    {hasTasks && !isSelected && <View style={styles.taskDot} />}
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
+    const renderTask = ({ item, index }) => (
+        <View style={styles.taskContainer}>
+            <Text style={styles.taskTitle}>{item.title}</Text>
+            <Text style={styles.taskDescription}>{item.description}</Text>
+            <Text style={styles.taskCategory}>{item.category}</Text>
+            <Text style={styles.taskTime}>{item.time}</Text>
+            <View style={styles.taskActions}>
+                <TouchableOpacity onPress={() => handleEditTask(index)}>
+                    <Text style={styles.taskActionText}>Editar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleDeleteTask(index)}>
+                    <Text style={styles.taskActionText}>Excluir</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 
   return (
-    <KeyboardAvoidingView style={styles.Background} behavior="padding">
-      <View>
-        <Image style={styles.Logo} resizeMode="contain" source={require('../../assets/images/argendex.png')} />
-      </View>
-
-      <ScrollView keyboardShouldPersistTaps="always" style={styles.container}>
-
-        <TextInput
-          style={styles.Inputs}
-          value={email}
-          placeholder="E-mail"
-          placeholderTextColor='#b8b8b8'
-          onChangeText={setEmail}
-        />
-
-        <TextInput
-          style={styles.Inputs}
-          placeholder="Senha"
-          value={Senha}
-          placeholderTextColor={'#b8b8b8'}
-          onChangeText={setSenha}
-        />
-
-        <TouchableOpacity style={styles.btnCriar} onPress={() => navigation.navigate('Calendario')}>
-          <Text style={styles.Txtbtn} /*onPress={handleLogin}*/ >Logar</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.btnvl} onPress={() => navigation.navigate('Cadastro')}>
-          <Text style={styles.Txtbtn}>Ainda não tem conta? Cadastre-se</Text>
-        </TouchableOpacity>
-
-      </ScrollView>
-    </KeyboardAvoidingView>
-  );
+    <View style={styles.taskInputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nova Tarefa"
+                    placeholderTextColor="grey"
+                    value={newTask}
+                    onChangeText={setNewTask}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Descrição"
+                    placeholderTextColor="grey"
+                    value={description}
+                    onChangeText={setDescription}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Categoria"
+                    placeholderTextColor="grey"
+                    value={category}
+                    onChangeText={setCategory}
+                />
+                <TouchableOpacity onPress={() => setShowTimePicker(true)}>
+                    <Text style={styles.timePickerText}>Escolher Horário: {time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</Text>
+                </TouchableOpacity>
+                {showTimePicker && (
+                    <DateTimePicker
+                        value={time}
+                        mode="time"
+                        display="default"
+                        onChange={(event, selectedTime) => {
+                            setShowTimePicker(false);
+                            if (selectedTime) {
+                                setTime(selectedTime);
+                            }
+                        }}
+                    />
+                )}
+                {selectedTaskIndex !== null ? (
+                    <TouchableOpacity onPress={editTask} style={styles.addButton}>
+                        <Text style={styles.addButtonText}>Editar</Text>
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity onPress={addTask} style={styles.addButton}>
+                        <Text style={styles.addButtonText}>Adicionar</Text>
+                    </TouchableOpacity>
+                )}
+            </View>
+      );
+      
 };
 
 export default Addtarefa;
--- argendex_cell/Pages/AddTarefa/Style_Add.js
+++ argendex_cell/Pages/AddTarefa/Style_Add.js
@@ -1,66 +1,106 @@
 import { ImageBackground, StyleSheet } from "react-native";
 
-export default style = StyleSheet.create({
-    Background: {
-        flex: 1,
-        backgroundColor: '#191515',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 30,
-
-    },
-
-    Logo: {
-        width: 500,
-        height: 250,
-        marginLeft:50,
-    },
-
-    container: {
-        flex: 1,
-        width: '90%',
-        marginLeft:50,
-    },
-
-    Inputs:{
-        backgroundColor: '#191515',
-        width: '90%',
-        marginBottom: 30,
-        borderRadius: 5,
-        padding: 10,
-        fontSize: 17,
-        borderRadius: 5,
-        padding: 10,
-        borderBottomWidth: 2,
-        borderColor: '#008000',
-        color: '#b8b8b8'
-    
-    },
-
-
-    submitTxt:{
-        color: '#fff',
-        fontSize: 18,
-    },
-    Txtbtn: {
-        color: '#fff',
-        fontSize: 18,
-    },
-
-    btnCriar: {
-        backgroundColor: 'green',
-        justifyContent: 'center',
-        marginTop: 20,
-        marginBottom: 15,
-        alignItems: 'center',
-        width: '90%',
-        height: 45,
-        borderRadius: 5,
-    },
-    btnvl: {
-        justifyContent: 'center',
-        marginLeft:18,
-        height: 50,
-        
-    },
+export default styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#191515',
+        },
+        calendario: {
+            backgroundColor: '#191515',
+        },
+        taskInputContainer: {
+            paddingHorizontal: 20,
+            marginVertical: 10,
+        },
+        input: {
+            borderColor: 'green',
+            borderWidth: 1,
+            marginBottom: 10,
+            paddingHorizontal: 10,
+            height: 40,
+            color: 'white',
+        },
+        timePickerText: {
+            paddingVertical: 10,
+            color: 'green',
+        },
+        taskContainer: {
+            padding: 10,
+            borderBottomWidth: 1,
+            borderBottomColor: 'grey',
+        },
+        taskTitle: {
+            fontSize: 16,
+            fontWeight: 'bold',
+            color: 'white',
+        },
+        taskDescription: {
+            fontSize: 14,
+            color: 'grey',
+        },
+        taskCategory: {
+            fontSize: 14,
+            color: 'green',
+        },
+        taskTime: {
+            fontSize: 14,
+            color: 'red',
+        },
+        addButton: {
+            backgroundColor: 'green',
+            padding: 10,
+            alignItems: 'center',
+            borderRadius: 5,
+        },
+        addButtonText: {
+            color: 'white',
+            fontWeight: 'bold',
+        },
+        taskDot: {
+            width: 5,
+            height: 5,
+            borderRadius: 2.5,
+            backgroundColor: 'green',
+            alignSelf: 'center',
+            marginTop: 2,
+        },
+        modalContainer: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        },
+        modalContent: {
+            backgroundColor: '#191515',
+            padding: 20,
+            borderRadius: 10,
+            width: '80%',
+            maxHeight: '80%',
+        },
+        modalTitle: {
+            fontSize: 20,
+            fontWeight: 'bold',
+            color: 'white',
+            marginBottom: 10,
+        },
+        closeButton: {
+            marginTop: 10,
+            backgroundColor: 'green',
+            padding: 10,
+            alignItems: 'center',
+            borderRadius: 5,
+        },
+        closeButtonText: {
+            color: 'white',
+            fontWeight: 'bold',
+        },
+        taskActions: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginTop: 10,
+        },
+        taskActionText: {
+            color: 'green',
+            fontWeight: 'bold',
+        },
 });
