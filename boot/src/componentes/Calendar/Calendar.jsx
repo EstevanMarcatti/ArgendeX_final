47d0af2327d801a1d6b98d77e896b4a4069e885f
Estevan Marcatti <marcattiestevan@gmail.com>
2024-10-31 10:48:22 -0300

    10:48

--- boot/src/componentes/Calendar/Calendar.jsx
+++ boot/src/componentes/Calendar/Calendar.jsx
@@ -22,6 +22,10 @@ const Calendar = () => {
   const [eventDescription, setEventDescription] = useState('');
   const [eventCategory, setEventCategory] = useState('');
   const userId = localStorage.getItem('ID'); // Obtenha o ID do usuário do localStorage
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [tasks, setTasks] = useState([]); // Estado para armazenar tarefas
+  const [editModalVisible, setEditModalVisible] = useState(false); // Estado para controlar o modal de edição
+  const [editedTask, setEditedTask] = useState(null); // Estado para armazenar a tarefa que está sendo editada
 
   const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
@@ -84,7 +88,6 @@ const Calendar = () => {
           setEvents(prevEvents => [...prevEvents, newEvent]);
         }
 
-        // Resetar formulário e fechar modais
         setEventTitle('');
         setEventTime('');
         setEventDescription('');
@@ -93,8 +96,15 @@ const Calendar = () => {
         setEventModalVisible(false);
         setModalVisible(false);
 
-        // Atualizar o modal do dia selecionado
-        setModalVisible(true);
+        // Mostrar a mensagem de confirmação
+        setShowConfirmation(true);
+
+        // Esperar um tempo antes de atualizar a página
+        setTimeout(() => {
+          setShowConfirmation(false); // Ocultar a mensagem
+          window.location.reload(); // Recarregar a página
+        }, 2000); // 2 segundos
+
       } catch (error) {
         console.error('Erro ao salvar evento:', error.message);
       }
@@ -109,7 +119,7 @@ const Calendar = () => {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ id }) // Passando o ID corretamente
-    });
+      });
       // Atualizar a lista de eventos local
       setEvents(events.filter(event => event.id !== id));
       setCurrentEvent(null);
@@ -119,10 +129,29 @@ const Calendar = () => {
     }
   };
 
+  const editTask = (updatedTask) => {
+    const updatedTasks = tasks.map(task =>
+      task.id === updatedTask.id ? updatedTask : task
+    );
+
+    setTasks(updatedTasks); // Atualiza o estado com as tarefas modificadas
+    setEditModalVisible(false); // Fecha o modal
+  };
+
   const handleShowTasks = (day) => {
     setSelectedDate(day + 1);
     setCurrentEvent(null);
     setModalVisible(true);
+  };
+
+  const handleEditTask = (task) => {
+    // Configura a tarefa a ser editada e abre o modal de edição
+    setCurrentEvent(task);
+    setEventTitle(task.title);
+    setEventTime(task.time);
+    setEventDescription(task.description);
+    setEventCategory(task.category);
+    setEventModalVisible(true);
   };
 
   const handlePrevMonth = () => {
@@ -147,27 +176,26 @@ const Calendar = () => {
 
   useEffect(() => {
     const fetchEvents = async () => {
-        const userId = localStorage.getItem('ID'); // Obtém o ID do usuário logado
-        if (!userId) {
-            console.error('Usuário não autenticado.');
-            return;
-        }
+      const userId = localStorage.getItem('ID'); // Obtém o ID do usuário logado
+      if (!userId) {
+        console.error('Usuário não autenticado.');
+        return;
+      }
 
-        try {
-            const response = await fetch(`http://localhost:8085/tasks?user_id=${userId}`);
-            if (!response.ok) {
-                throw new Error('Erro na resposta da API');
-            }
-            const data = await response.json();
-            setEvents(data);
-        } catch (error) {
-            console.error('Erro ao carregar tarefas:', error);
+      try {
+        const response = await fetch(`http://localhost:8085/tasks?user_id=${userId}`);
+        if (!response.ok) {
+          throw new Error('Erro na resposta da API');
         }
+        const data = await response.json();
+        setEvents(data);
+      } catch (error) {
+        console.error('Erro ao carregar tarefas:', error);
+      }
     };
 
     fetchEvents();
-}, [currentMonth, currentYear]);
-
+  }, [currentMonth, currentYear]);
 
   return (
     <div className="calendar">
@@ -218,14 +246,7 @@ const Calendar = () => {
                       <div>{task.description}</div>
                       <div>Categoria: {task.category}</div>
                       <div className="modal-calendar-buttons">
-                        <button onClick={() => {
-                          setCurrentEvent(task);
-                          setEventTitle(task.title);
-                          setEventTime(task.time);
-                          setEventDescription(task.description);
-                          setEventCategory(task.category);
-                          setEventModalVisible(true);
-                        }}>Editar</button>
+                        <button onClick={() => handleEditTask(task)}>Editar</button>
                         <button onClick={() => handleDeleteEvent(task.id)}>Excluir</button>
                       </div>
                     </div>
@@ -290,6 +311,64 @@ const Calendar = () => {
               </button>
             </div>
           </div>
+        </div>
+      )}
+
+      {eventModalVisible && (
+        <div className="modal-calendar">
+          <div className="modal-calendar-content">
+            <h2>{currentEvent ? 'Editar Tarefa' : 'Adicionar Tarefa'}</h2>
+            <input
+              type="text"
+              value={eventTitle}
+              onChange={(e) => setEventTitle(e.target.value)}
+              placeholder="Título"
+            />
+            <input
+              type="time"
+              value={eventTime}
+              onChange={(e) => setEventTime(e.target.value)}
+            />
+            <textarea
+              value={eventDescription}
+              onChange={(e) => setEventDescription(e.target.value)}
+              placeholder="Descrição"
+            />
+            <input
+              type="text"
+              value={eventCategory}
+              onChange={(e) => setEventCategory(e.target.value)}
+              placeholder="Categoria"
+            />
+            <div className="modal-calendar-buttons">
+              <button onClick={handleSaveEvent}>
+                {currentEvent ? 'Salvar' : 'Adicionar'}
+              </button>
+              {currentEvent && (
+                <button
+                  className="cancel"
+                  onClick={() => handleDeleteEvent(currentEvent.id)}
+                >
+                  Excluir
+                </button>
+              )}
+              <button
+                className="cancel"
+                onClick={() => {
+                  setEventModalVisible(false);
+                  setCurrentEvent(null); // Reseta a tarefa atual ao fechar
+                }}
+              >
+                Fechar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {showConfirmation && (
+        <div className="confirmation-container">
+          <p className="confirmation-text">Tarefa adicionada com sucesso!</p>
         </div>
       )}
     </div>
